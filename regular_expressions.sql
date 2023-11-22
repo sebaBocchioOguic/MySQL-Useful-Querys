0e0394499bc47611cf0721e7cab84a4b18ce8cc3
Sebastian Bocchio Oguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2023-11-22 00:21:20 -0300

    Update regular_expressions.sql

--- regular_expressions.sql
+++ regular_expressions.sql
@@ -4,9 +4,12 @@
 
 /* Obtain results of City column where City doesn't start with vowels */
 
-SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[aeiou]'
+  SELECT DISTINCT CITY FROM STATION WHERE CITY NOT REGEXP '^[aeiou]'
 
 
 /* Obtain results of City column where City starts AND ends with vowels */
+  /* Option 1 */
+  SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiou]' AND CITY REGEXP '[aeiou]$'
 
-SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '^[aeiou]' AND CITY REGEXP '[aeiou]$'
+  /* Option 2 */
+  SELECT DISTINCT CITY FROM STATION WHERE CITY RLIKE '^[AEIOU].*[aeiou]$'
