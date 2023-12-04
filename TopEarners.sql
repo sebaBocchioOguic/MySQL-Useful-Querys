3a2e4b754b952ec9e49507fdce15eaa7fb5d53fe
Sebastian Bocchio Oguic <60882728+sebaBocchioOguic@users.noreply.github.com>
2023-12-04 09:00:22 -0300

    Update and rename TopEarners to TopEarners.sql

--- TopEarners.sql
+++ TopEarners.sql
@@ -2,7 +2,7 @@
 We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
 */
 
-/* Solution */
+/* Solution 1 - Not Optimal but easier to read and analize */
 
 /* Set Variable with maximum value */
 SET @maximum = (SELECT max(months * salary) FROM Employee);
@@ -10,3 +10,8 @@ SET @maximum = (SELECT max(months * salary) FROM Employee);
 /* Select the registers that matches with the searched maximum value */
 SELECT @maximum, count(*) FROM Employee
 WHERE (months * salary) = @maximum
+
+
+/* Solution 2 - Optimal in one line */
+SELECT MAX(MONTHS * SALARY), COUNT(*) FROM EMPLOYEE WHERE (MONTHS * SALARY) = (SELECT MAX(MONTHS * SALARY) FROM EMPLOYEE);
+
